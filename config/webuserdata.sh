0632f1b81436b2a19b95150353c25997f60ae27d
Jason Harris <jasonharris@Jasons-MacBook-Pro-2.local>
2021-08-24 18:22:33 -0400

    removed golang role

--- config/webuserdata.sh
+++ config/webuserdata.sh
@@ -3,8 +3,7 @@
 apt-get -y update
 apt-get -y install ansible
 ansible-pull -U https://github.com/jason-oss/playbooks.git
-ansible-galaxy install fubarhouse.golang
-ansible-galaxy install ansiblebit.awscli
+
 
 #export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
 #export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
